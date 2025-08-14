35666dcedfde6c566d3b98d7b08df17f3608dea8
Kaushal Rana <kaushal2411rana@gmail.com>
2025-08-14 19:49:17 +0530

    Create Add-two-numbers.c
    This is a simple code in which first I take two variable with integer data type and assign them some random values and print their sum.

---
+++ Add-two-numbers.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main() {
+    int a =5,b=24; // In this line we declare and initialize two integer variables a and b
+    printf("The sum of the number %d and %d is %d", a, b, a + b); // This line prints the sum of a and b
+    return 0;
+}
