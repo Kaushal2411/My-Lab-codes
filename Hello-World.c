7638e4d02afe41ab2355a30b1d9d9de54b2fcde0
Kaushal Rana <kaushal2411rana@gmail.com>
2025-08-14 19:42:58 +0530

    Create Hello-World.c
    This is a simple code which is used to print Hello World as an output. It uses "printf" function of C language to print Hello World.

---
+++ Hello-World.c
@@ -0,0 +1,5 @@
+#include <stdio.h> // This line includes the standard input-output library for C
+int main () { // The main function is the entry point of the program
+    printf("Hello World \n");
+    return 0; // The program returns 0 to indicate successful execution
+}
