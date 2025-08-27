bc37c400c417809a0ba1740e1ada6b0845966d7a
Kaushal2411 <kaushal2411rana@gmail.com>
2025-08-27 10:24:48 +0530

    Opeartors

---
+++ Operators.c
@@ -0,0 +1,17 @@
+// Relational operators in c
+#include <stdio.h>
+int main () {
+    int x,y;
+    printf("Enter a number\n");
+    scanf("%d",&x);
+    printf("Enter another number\n");
+    scanf("%d",&y);
+    printf("%d\n",x>=y);
+    printf("%d\n",x<=y);
+    printf("%d\n",x>y);
+    printf("%d\n",x<y);
+    printf("%d\n",x==y);
+    printf("%d\n",x!=y);
+    return 0;
+    
+}
