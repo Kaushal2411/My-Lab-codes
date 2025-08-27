7e5e703d7d2b8a55e8f368903dcab385587eecda
Kaushal2411 <kaushal2411rana@gmail.com>
2025-08-27 09:19:48 +0530

    Code to convert days to years, months and days

---
+++ days-to-year.c
@@ -0,0 +1,13 @@
+// W.A.P to convert numbers of days to years, months and days.
+#include <stdio.h>
+int main () {
+    int days, years, months, remaining_days;
+    printf("Enter number of days\n");
+    scanf("%d", &days);
+    years = days / 365;
+    remaining_days = days % 365;            
+    months = remaining_days / 30;           
+    remaining_days = remaining_days % 30;   
+    printf("%d days is equivalent to %d years, %d months and %d days\n", days, years, months, remaining_days);
+    return 0;
+}
