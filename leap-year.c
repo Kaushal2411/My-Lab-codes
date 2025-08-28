f04fb239c2086a1565f876e0e6c9d02157a18d3d
Kaushal2411 <kaushal2411rana@gmail.com>
2025-08-28 12:09:28 +0530

    Code to check if the entered year is a leap year or not

---
+++ leap-year.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main() {
+    int a; 
+    printf("Enter year ");
+    scanf("%d", &a);
+    if (a%4 ==0 && a%100 != 0 || a%400 == 0) {
+        printf("This is a leap year \n");
+    }
+    else{
+        printf("This is not a leap year \n");
+    }
+    return 0;
+}
